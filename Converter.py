cdd085404696085e21c94cb858be8f1c2f36c8ba
Sova63 <osnova-vsego@yandex.ru>
2024-03-17 10:24:47 +0300

    Добавил файл

---
+++ Converter.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+import User
+
+def ConvertJSONToClass(jsonObject,objectName):
+	obj = namedtuple(objectName.jsonObject.keys())(*jsonObject.value())
+	return obj
+def ConvertUserJSONToClass(jsonObject):
+	return User(jsonObject["id"],jsonObject["name"],jsonObject["isManager"])
+
+# Converter.py
+"""
+import json
+
+def ConvertJSONToClass(json_data, objectName):
+	data = json.loads(json_data)
+	obj = object()
+	for key, value in data.items():
+		setattr(obj, key, value)
+	return obj
+
+"""
